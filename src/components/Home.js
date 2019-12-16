0ce009a58715b4edc41094809850397ea96d3cce
JimmeeX <james.lin1997@hotmail.com>
2019-12-16 22:41:09 +1100

    add logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import sample1 from '../images/sample.jpg';
+import mainLogo from '../images/main_logo.png';
 
 const Home = () => (
   <div id='home'>
-    <img src={sample1} alt='sample1' />
+    <div className='main-logo-wrapper'>
+      <img src={mainLogo} alt='main_logo'></img>
+    </div>
+    <div className='carousel-wrapper'>
+      <img src={sample1} alt='sample1' />
+    </div>
   </div>
 );
 
--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { HashLink as Link } from 'react-router-hash-link';
 
-import logo from '../images/logo.jpg';
+import logo from '../images/logo_text_eng.png';
 
 const Header = () => (
   <BrowserRouter>
     <div className='header'>
-      <img className='header-logo' src={logo} height='50px' alt='logo' />
+      <div className='header-logo-wrapper'>
+        <Link smooth to="#home">
+          <img className='header-logo' src={logo} alt='logo' />
+        </Link>
+      </div>
       <ul className='header-menu'>
         <li><Link smooth to="#home">Home</Link></li>
         <li><Link smooth to="#about">About</Link></li>
