44c5e1ab1c321d02033c5f3da8484d3d799ce904
JimmeeX <james.lin1997@hotmail.com>
2019-12-17 13:15:16 +1100

    add map + pdf

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,6 +19,7 @@ const Contact = () => (
         </div>
       </div>
       <div className='section-extra-wrapper'>
+        <iframe title="wonton-warrior-map" width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=135A%20Burwood%20Rd%2C%20Burwood%20NSW%202134&t=&z=17&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
       </div>
     </div>
   </div>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import menuPDF from '../menu.pdf';
+
 const Menu = () => (
   <div id='menu' className='section'>
     <div className='section-layout'>
@@ -7,6 +9,7 @@ const Menu = () => (
       <div className='section-content-wrapper'>
         <h1>Menu</h1>
         <p>Wonton Warrior specialises in unique, freshly hand-crafted wontons. We also offer a comprehensive range of other Chinese delicacies including dumplings noodles and specialty dishes to suit all taste palettes and appetites.</p>
+        <a href={menuPDF}><p>Download Menu</p></a>
       </div>
     </div>
   </div>
