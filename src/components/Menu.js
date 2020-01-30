db21e2b11fa3de96943897822e5976606bf37585
James Lin <james.lin1997@hotmail.com>
2020-01-30 20:59:30 +1100

    add more seo; minor fixes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,7 +10,7 @@ const Menu = () => (
       <Gallery />
       <div className='section-content-wrapper'>
         <h1>Menu</h1>
-        <p>Wonton Warrior specialises in unique, freshly hand-crafted wontons. We also offer a comprehensive range of other Chinese delicacies including dumplings noodles and specialty dishes to suit all taste palettes and appetites.</p>
+        <p>Wonton Warrior specialises in unique, freshly hand-crafted wontons. We also offer a comprehensive range of other Chinese delicacies including dumplings, noodles and specialty dishes to suit all taste palettes and appetites.</p>
         <p><a href={menuPDF} className='menu-link'>Download Menu</a></p>
       </div>
     </div>
